d9b7537701a674d4ff98e8d34c2d8bbcd8e5f409
James Woodward <jamesrobertwoodward@gmail.com>
2022-03-31 11:00:25 -0500

    working on edit movie, partial functionality

--- js/jquery.js
+++ js/jquery.js
@@ -1,43 +1,44 @@
 
-//URL of our movie database hosted on Glitch.me
+
 let moviesUrl = "https://receptive-different-edam.glitch.me/movies";
+
 
 getMoviesDB(moviesUrl);
 
-//Click event to execute addMovie function
+createDropdown();
+
 $('#add-submit').click(function () {
-   addMovie();
+    addMovie();
 });
 
-//Click event to execute deleteMovie function
 $('#delete-submit').click(function () {
     let item = $('#delete-item').val();
     deleteMovie(item);
 });
 
-//Click event to execute editMovie function
 $('#edit-submit').click(function () {
     editMovie();
 });
 
-createDropdown();
+$('#select-movie').change(function(){
+    let title = $(this).val()
+    populateEditForm(title);
+});
 
-//Function which takes a string and returns string with the first letter of each word capitalized
 function fixCase(string) {
-        if (typeof (string) === 'string' && string !== '') {
-            let names = [];
-            let fixedNames = '';
-            names = string.split(" ");
-            for (let i = 0; i < names.length; i++) {
-                names.push(names[i].charAt(0).toUpperCase() + names[i].slice(1).toLowerCase());
-                fixedNames = fixedNames + names.pop() + ' ';
-            }
-            console.log(fixedNames);
-            return fixedNames;
+    if (typeof (string) === 'string' && string !== '') {
+        let names = [];
+        let fixedNames = '';
+        names = string.split(" ");
+        for (let i = 0; i < names.length; i++) {
+            names.push(names[i].charAt(0).toUpperCase() + names[i].slice(1).toLowerCase());
+            fixedNames = fixedNames + names.pop() + ' ';
         }
+        console.log(fixedNames);
+        return fixedNames;
+    }
 }
 
-//Function which takes a number (numerical rating) and replaces it with stars. Limited to 5
 function starRating (num){
     let stars = '';
     for(let i = 1; i <= 5; i++) {
@@ -46,15 +47,13 @@ function starRating (num){
     return stars;
 }
 
-//Function which will delete a movie from the database
 function deleteMovie(item) {
-        return fetch(moviesUrl + '/' + item, {
-            method: 'delete'
-        })
-            .then(response => response.json());
+    return fetch(moviesUrl + '/' + item, {
+        method: 'delete'
+    })
+        .then(response => response.json());
 }
 
-//Function which takes user input through a form and adds a new movie object to the database
 function addMovie() {
     let data = { title:  $('#add-title').val(),
         genre:  $('#add-genre').val(),
@@ -76,20 +75,48 @@ function addMovie() {
         .catch((error) => {
             console.error('Error:', error);
         });
+    createDropdown();
 }
 
-//Function which takes all movie names within the database and places them into a dropdown list
 function createDropdown() {
     fetch(moviesUrl)
         .then(response => response.json())
         .then(data => {
-            // console.log(data)
-            for (let i = 0; i < (data.length - 1); i++) {
-                let movieTitle = fixCase(data[i].title);
+
+            console.log(data)
+
+            for (let i = 0; i < (data.length); i++) {
+                let movieTitle = data[i].title;
+                let movieGenre = data[i].genre;
+                let movieYear = data[i].year;
+                let moviePlot = data[i].plot;
+                let movieRating = starRating(data[i].rating);
                 if(data[i].title !== undefined) {
-                    $('#selectMovie').append(`
-                    <li><a class="dropdown-item" href="#">${movieTitle}</a></li>
+                    $('#select-movie').append(`
+                    <option> ${movieTitle} </option>
                     `)
+                }
+            }
+        });
+}
+
+function populateEditForm(title) {
+    fetch(moviesUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            for (let i = 0; i < (data.length); i++) {
+                let movieTitle = fixCase(data[i].title);
+                let movieGenre = fixCase(data[i].genre);
+                let movieYear = data[i].year;
+                let moviePlot = data[i].plot;
+                let movieRating = starRating(data[i].rating);
+                if(data[i].title === title) {
+                    $('#edit-title').val(`${movieTitle}`)
+                    $('#edit-genre').val(`${movieGenre}`)
+                    $('#edit-year').val(`${movieYear}`)
+                    $('#edit-plot').val(`${moviePlot}`)
+                    $('#edit-rating').val(`${movieRating}`)
                 }
             }
         });
@@ -118,16 +145,16 @@ function editMovie() {
         });
 }
 
-//Function which takes all the movies' data from the database and renders it
+
 function getMoviesDB(url) {
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
 
-            // console.log(data)
+            console.log(data)
 
-            for (let i = 0; i < (data.length - 1); i++) {
+            for (let i = 0; i < (data.length); i++) {
                 let movieTitle = fixCase(data[i].title);
                 let movieGenre = fixCase(data[i].genre);
                 let movieYear = data[i].year;
